21a3473f0fa2d765a916a77923c8f9c6ae174707
Vikram Nilaji <97115025+vikramNilaji@users.noreply.github.com>
2024-11-22 19:24:58 +0530

    add

--- XO/src/Data.jsx
+++ XO/src/Data.jsx
@@ -3,42 +3,43 @@ const Data = () => {
     {
       id: 1,
       question: "What is the capital of France?",
-      answer: "Paris"
+      answer: "Paris",
     },
     {
       id: 2,
       question: "What is 5 + 7?",
-      answer: "12"
+      answer: "12",
     },
     {
       id: 3,
       question: "Who wrote 'Romeo and Juliet'?",
-      answer: "William Shakespeare"
+      answer: "William Shakespeare",
     },
     {
       id: 4,
       question: "What is the largest planet in our solar system?",
-      answer: "Jupiter"
+      answer: "Jupiter",
     },
     {
       id: 5,
       question: "What is the boiling point of water?",
-      answer: "100°C"
-    }
+      answer: "100°C",
+    },
   ];
 
-  return <>
-  {data.map((value,index)=>{
-
-    return(
-    <div key={index}>
-
-    <h1>{value.id}</h1>
-    <h1>{value.question}</h1>
-    <h1>{value.answer}</h1>
-    </div>
-
-  )})}</>;
+  return (
+    <>
+      {data.map((value, index) => {
+        return (
+          <div key={index}>
+            <h1>{value.id}</h1>
+            <h1>{value.question}</h1>
+            <h1>{value.answer}</h1>
+          </div>
+        );
+      })}
+    </>
+  );
 };
 
 export default Data;
