6964e1bc0cfa52ac3973d253629815fc38fe85a7
Vikram Nilaji <97115025+vikramNilaji@users.noreply.github.com>
2024-11-30 00:17:58 +0530

    add

--- XO/src/Component/Accordian.jsx
+++ XO/src/Component/Accordian.jsx
@@ -9,18 +9,18 @@ const Accordian=()=>{
 
       <h1>Accordian</h1>
 
-      {data.map((value,index)=>
+      {data && data.length > 0 ?  data.map((value,index)=>
         
 
           <div key={index}>
           <h1>{value.id}</h1>
-          <h1>{value.question}</h1>
+          {/* <h1>{value.question}</h1> */}
           <h1>{value.answer}</h1>
 
-          </div>
+          </div> ) : <div> Nothing is here</div>
 
         
-        )}
+        }
     </>
   )
 
