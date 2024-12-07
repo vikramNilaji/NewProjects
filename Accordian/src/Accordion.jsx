c752a7afb359f3a3b3ad0a39c60b89846818ed5f
vikramNilaji <vik.nilaji@gmail.com>
2024-12-07 12:12:17 +0530

    add

--- Accordian/src/Accordion.jsx
+++ Accordian/src/Accordion.jsx
@@ -4,14 +4,22 @@ import { useState } from "react";
 const Accordion = () => {
   const [dataId,setDataId]=useState(null)
 
-  const[enableMultiSelection,setEnableMultiSelection]=useState()
-  function handleButton(Item) {
+  const[enableMultiSelection,setEnableMultiSelection]=useState(false)
+  const[multiple,setMultiple]=useState([])
+  function handleSingleSelection(Item) {
     console.log(Item);
     setDataId(Item === dataId ?  null : Item)
   }
+
+  function handleMultiSelection(dataId){
+    let copyMultiple=[...multiple]
+     const findIndexOfCurrentId=copyMultiple.indexOf(dataId)  
+     console.log(findIndexOfCurrentId);
+     if()
+  }
   return (
     <>
-    <button style={{height: "100px" ,width: "200px"}}>EnableMultiSelection</button>
+    <button onClick={()=> setEnableMultiSelection(!enableMultiSelection)} style={{height: "60px" ,width: "200px"}}  >EnableMultiSelection</button>
 
       {data.map((dataItems, index) => {
         return (
@@ -19,7 +27,7 @@ const Accordion = () => {
             {/* <h1>{dataItems.id}</h1>
             <h1>{dataItems.Question}</h1> */}
             <h1>{dataItems.Question}</h1>
-            <h1 onClick={()=>handleButton(dataItems.id)}>+ </h1>
+            <h1 onClick={ enableMultiSelection? ()=>handleMultiSelection(dataItems.id): ()=>handleSingleSelection(dataItems.id)}>+ </h1>
             <div> {dataId === dataItems.id ? <div>{dataItems.Answer}</div> : null}</div>
           </div>
         );
