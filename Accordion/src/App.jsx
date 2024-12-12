e9eaf8592107c6cfac3f94ccb727a112ce048ca1
Vikram Nilaji <97115025+vikramNilaji@users.noreply.github.com>
2024-12-12 06:27:23 +0530

    add

--- Accordion/src/App.jsx
+++ Accordion/src/App.jsx
@@ -1,27 +1,31 @@
-import Data from "./Data.js"
+import Data from "./Data.js";
+import { useState } from "react";
 function App() {
+  const [selected, setSelected] = useState(null);
+  const [EnableMultiSelection,setEnableMultiSelection]=useState(false)
 
-  function HandleSingleButton(){
-    return(
-      
-
-    )
-
+  function HandleSingleButton(CurrentId) {
+    return setSelected(CurrentId===selected? null:CurrentId);
   }
-  
+
+  function HandleMultiSelection(CurrentId) {
+    setSelected(CurrentId)
+  }
+
   return (
     <>
-     <div>
-      {Data.map((dataItems,index)=>(
-        <div key={index}>
-        <h1>{dataItems.Question}</h1>
-        <h1 onClick={HandleSingleButton}>+</h1>
-        </div>
-       
-      ))}
-     </div>
+      <div>
+      <button onClick={()=>setEnableMultiSelection(!EnableMultiSelection)}>EnableMultiSelection</button>
+        {Data.map((dataItems, index) => (
+          <div key={index}>
+            <h1>{dataItems.Question}</h1>
+            <h1 onClick={EnableMultiSelection ? ()=>HandleMultiSelection(dataItems.id):() => HandleSingleButton(dataItems.id)}>+</h1>
+            {dataItems.id === selected ? <h1>{dataItems.Answer}</h1> : null}
+          </div>
+        ))}
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
