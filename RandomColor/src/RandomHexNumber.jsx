234ac2417dad39355ae17088d92723e65ca68bfd
Vikram Nilaji <97115025+vikramNilaji@users.noreply.github.com>
2024-12-19 07:59:18 +0530

    add

--- RandomColor/src/RandomHexNumber.jsx
+++ RandomColor/src/RandomHexNumber.jsx
@@ -1,26 +1,27 @@
 import { useState } from "react";
 
 const RandomHexNumber = () => {
-  const [color, setColor] = useState('#ffffff'); 
+  const [color, setColor] = useState("#ffffff")
+  const Hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+  const RandomItems = ['#']
 
-  function HexColorChangeButton() {
-    const Hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
-    let RandomHexNumber = ["#"]; 
-    
+  function HandleColorButton() {
     for (let i = 0; i < 6; i++) {
-      const RandomIndex = Math.floor(Math.random() * Hex.length);
-      RandomHexNumber.push(Hex[RandomIndex]);
+      const RandomIndex = Math.floor(Math.random() * Hex.length)
+      RandomItems.push(Hex[RandomIndex])
+      setColor(RandomItems.join(""))
+
+
     }
-    
-    setColor(RandomHexNumber.join(""));
+
+
   }
 
   return (
-    <div style={{ backgroundColor: color }}>
-      <button onClick={HexColorChangeButton}>Change Hex Color</button>
+    <div style={{ backgroundColor: color, height: "100vh", width: "100vw" }}>
+      <button onClick={HandleColorButton}>ChangeColor</button>
     </div>
   );
 };
 
 export default RandomHexNumber;
-
