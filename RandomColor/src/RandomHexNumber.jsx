8a4085b2679814720e13cd77760ceab4948530eb
vikramNilaji <vik.nilaji@gmail.com>
2024-12-19 14:45:58 +0530

    add

--- RandomColor/src/RandomHexNumber.jsx
+++ RandomColor/src/RandomHexNumber.jsx
@@ -3,7 +3,9 @@ import { useState } from "react";
 const RandomHexNumber = () => {
   const [color, setColor] = useState("#ffffff")
   const Hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
-  
+  // Hex.sort(() => Math.random()-0.3 );
+
+  Hex.sort(()=>Math.random()-0.5)
 
 
   const RandomItems = ['#']
