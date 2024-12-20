223d30ceb57aef50751280aa709f8c1e52d6245f
vikramNilaji <vik.nilaji@gmail.com>
2024-12-20 13:35:52 +0530

    add

---
+++ StarRating/src/StarRating.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import {FaStar} from "react-icons/fa"
+import { useState } from "react";
+import "./styles.css"
+
+const StarRating = ({ NoOfStars = 5 }) => {
+  const [rating, setRating] = useState(0);
+  const [hover, setHover] = useState(0);
+
+  function handleClick(getCurrentIndex) {
+    console.log(getCurrentIndex);
+    setRating(getCurrentIndex)
+  }
+
+  function handleMouseEnter(getCurrentIndex) {
+    console.log(getCurrentIndex);
+    setHover(getCurrentIndex)
+  }
+
+  function handleMouseLeave() {
+     setHover(rating)
+  }
+
+  return (
+    <div className="star-rating">
+      {[...Array(NoOfStars)].map((_, index) => {
+        index += 1;
+
+        return (
+          <FaStar
+            key={index}
+            className={index <= (hover||rating) ? 'active' : 'inactive'}
+            onClick={() => handleClick(index)} 
+            onMouseMove={() => handleMouseEnter(index)}
+            onMouseLeave={() => handleMouseLeave()}
+            size={40}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+export default StarRating;
