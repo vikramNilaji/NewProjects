b78cc5ea0e400c1d54e275b5c3c30fc9e835890c
Vikram Nilaji <97115025+vikramNilaji@users.noreply.github.com>
2024-12-20 21:55:53 +0530

    add

--- StarRating/src/StarRating.jsx
+++ StarRating/src/StarRating.jsx
@@ -29,7 +29,7 @@ const StarRating = ({ NoOfStars = 5 }) => {
         return (
           <FaStar
             key={index}
-            className={index <= (hover||rating) ? 'active' : 'inactive'}
+            className={index <= (hover||rating) ? 'active' : 'inactive  '}
             onClick={() => handleClick(index)} 
             onMouseMove={() => handleMouseEnter(index)}
             onMouseLeave={() => handleMouseLeave()}
