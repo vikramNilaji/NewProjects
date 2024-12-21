d01c36c6a8d1f7edeead02073c0e45ed9a72436a
Vikram Nilaji <97115025+vikramNilaji@users.noreply.github.com>
2024-12-22 00:32:57 +0530

    add

---
+++ StarRating1/src/App.jsx
@@ -0,0 +1,16 @@
+
+import StarRating from './StarRating'
+
+function App() {
+ 
+
+  return (
+    <>
+
+    <StarRating Numbers={10}></StarRating>
+     
+    </>
+  )
+}
+
+export default App
---
+++ StarRating1/src/StarRating.jsx
@@ -0,0 +1,47 @@
+import { FaStar } from "react-icons/fa";
+import "./App.css"
+
+import { useState } from "react";
+
+const StarRating = ({ Numbers = 5 }) => {
+
+
+ const [hover,setHover]=useState(0)
+ const[rating,setRating]=useState(0)
+
+
+
+  function OnclickHandle(currentIndex) {
+    console.log(currentIndex);
+    setRating(currentIndex)
+  }
+
+  function OnMouseEnterHandle(currentIndex) {
+    console.log(currentIndex);
+    setHover(currentIndex)
+  }
+
+  function OnMouseLeaveHandle() {
+    setHover(rating)
+  }
+
+  return (
+    <>
+      {[...Array(Numbers)].map((_, index) => {
+        index += 1;
+        return (
+          <FaStar
+            key={index}
+            size={50}
+            className={index <= (hover||rating)? 'active' : 'inactive'}
+            onClick={() => OnclickHandle(index)}
+            onMouseEnter={() => OnMouseEnterHandle(index)}
+            onMouseLeave={() => OnMouseLeaveHandle()}
+          />
+        );
+      })}
+    </>
+  );
+};
+
+export default StarRating;
