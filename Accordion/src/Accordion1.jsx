d816b7ddedc2be24e2cd41103d26053b4fefd61d
Vikram Nilaji <97115025+vikramNilaji@users.noreply.github.com>
2025-01-29 07:09:04 +0530

    add

--- Accordion/src/Accordion1.jsx
+++ Accordion/src/Accordion1.jsx
@@ -1,9 +1,10 @@
 import { useState } from "react";
 import Data from "./Data.js";
+import "./style.css"
 
 const Accordion1 = () => {
   const [idSelection, setIdSelection] = useState([]);
-
+  const [enableMultiSelection, setEnableMultiSelection] = useState(false);
 
   function HandleSingleSelection(DataItemId) {
     setIdSelection(idSelection.includes(DataItemId) ? [] : [DataItemId]);
@@ -11,15 +12,39 @@ const Accordion1 = () => {
     console.log(idSelection);
   }
 
+  function HandleMultiSelection(DataItemId) {
+    if (idSelection.includes(DataItemId)) {
+      setIdSelection(idSelection.filter((id) => id !== DataItemId));
+    }
+
+    else{
+      setIdSelection([...idSelection,DataItemId])
+    }
+  }
+
   return (
-    <>
+    <>  <div className="background">
+      <button  className= "button" onClick={() => setEnableMultiSelection(!enableMultiSelection)}>
+        {" "}
+        {enableMultiSelection
+          ?  "DisableMultiSelection" : "EnableMultiSelection"
+           }{" "} 
+      </button>
+
+      <div className="content">
       {Data.map((DataItems, index) => (
-        <div key={index}>
+        <div key={index} className="Content">
           <h3>
             {DataItems.id} {")"}
           </h3>
           <h3>{DataItems.Question}</h3>{" "}
-          <h2 onClick={() => HandleSingleSelection(DataItems.id)}>
+          <h2
+            onClick={
+              enableMultiSelection
+                ? 
+                () => HandleMultiSelection(DataItems.id) : () => HandleSingleSelection(DataItems.id)
+            }
+          >
             {idSelection.includes(DataItems.id) ? "-" : "+"}
           </h2>
           {idSelection.includes(DataItems.id) ? (
@@ -27,6 +52,8 @@ const Accordion1 = () => {
           ) : null}
         </div>
       ))}
+      </div>
+      </div>
     </>
   );
 };
