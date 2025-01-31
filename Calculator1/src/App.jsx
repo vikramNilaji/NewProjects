e738bfd878c67a49b8e40067e4456a151cd530cf
Vikram Nilaji <97115025+vikramNilaji@users.noreply.github.com>
2025-01-31 05:31:29 +0530

    add

--- Calculator1/src/App.jsx
+++ Calculator1/src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  let Numbers = [
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [9, 0, "+", "-"],
+    ["x", "/", "=", "C"],
+  ];
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {Numbers.map((Num, rowIndex) => (
+        <ul key={rowIndex}>
+          {Num.map((num, colIndex) => (
+            <li key={colIndex}>{num}</li>
+          ))}
+        </ul>
+      ))}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
