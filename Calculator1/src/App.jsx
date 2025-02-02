376c8c7852193812831c49cf876f083ad86a0201
Vikram Nilaji <97115025+vikramNilaji@users.noreply.github.com>
2025-02-03 03:38:53 +0530

    add

--- Calculator1/src/App.jsx
+++ Calculator1/src/App.jsx
@@ -4,6 +4,19 @@ import "./App.css";
 function App() {
   const [count, setCount] = useState(0);
 
+  const [EnterInput,setEnterInput]=useState()
+  const [text,setText]=useState()
+
+
+  function HandleInput(e){
+         setText( e.target.value)
+  }
+
+  function HandleOnClick(){
+
+    setEnterInput(text)
+  }
+
   let Numbers = [
     [1, 2, 3, "+"],
     [4, 5, 6, "-"],
@@ -23,13 +36,13 @@ function App() {
     // </>
 
     <>
-    <div className="Input"><input type="text"/></div>
+    <div className="Input" value={text} onChange={HandleInput()}><input type="text"/></div>
     <div className="Main">
       {Numbers.map((Number1, index1) => (
         <div key={index1} className="Content">
           <ul>            
-            {Number1.map((Number2, index2) => (
-              <li key={index2}>{Number2}</li>
+            {Number1.map((EnterInput, index2) => (
+              <li key={index2} onClick={HandleOnClick}>{EnterInput}</li>
             ))}
           </ul>
         </div>
