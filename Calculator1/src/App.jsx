d43c8f9749e0e5045f6e88a4c834906ebeb214ba
vikramNilaji <vik.nilaji@gmail.com>
2025-02-03 11:08:26 +0530

    add

--- Calculator1/src/App.jsx
+++ Calculator1/src/App.jsx
@@ -4,17 +4,15 @@ import "./App.css";
 function App() {
   const [count, setCount] = useState(0);
 
-  const [EnterInput,setEnterInput]=useState()
-  const [text,setText]=useState()
+  const [EnterInput, setEnterInput] = useState();
+  const [text, setText] = useState();
 
-
-  function HandleInput(e){
-         setText( e.target.value)
+  function HandleInput(e) {
+    setText(e.target.value);
   }
 
-  function HandleOnClick(){
-
-    setEnterInput(text)
+  function HandleOnClick() {
+    setEnterInput(text);
   }
 
   let Numbers = [
@@ -25,28 +23,22 @@ function App() {
   ];
 
   return (
-    // <>
-    //   {Numbers.map((Num, rowIndex) => (
-    //     <ul key={rowIndex}>
-    //       {Num.map((num, colIndex) => (
-    //         <li key={colIndex}>{num}</li>
-    //       ))}
-    //     </ul>
-    //   ))}
-    // </>
-
     <>
-    <div className="Input" value={text} onChange={HandleInput()}><input type="text"/></div>
-    <div className="Main">
-      {Numbers.map((Number1, index1) => (
-        <div key={index1} className="Content">
-          <ul>            
-            {Number1.map((EnterInput, index2) => (
-              <li key={index2} onClick={HandleOnClick}>{EnterInput}</li>
-            ))}
-          </ul>
-        </div>
-      ))}
+      <div className="Input" value={text} onChange={HandleInput()}>
+        <input type="text" />
+      </div>
+      <div className="Main">
+        {Numbers.map((Number1, index1) => (
+          <div key={index1} className="Content">
+            <ul>
+              {Number1.map((EnterInput, index2) => (
+                <li key={index2} onClick={HandleOnClick}>
+                  {EnterInput}
+                </li>
+              ))}
+            </ul>
+          </div>
+        ))}
       </div>
     </>
   );
