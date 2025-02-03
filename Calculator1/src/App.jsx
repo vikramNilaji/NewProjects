733c3116c309201e107cd549ebde51b98a310261
vikramNilaji <vik.nilaji@gmail.com>
2025-02-03 13:04:57 +0530

    add

--- Calculator1/src/App.jsx
+++ Calculator1/src/App.jsx
@@ -1,47 +1,29 @@
 import { useState } from "react";
 import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0);
-
-  const [EnterInput, setEnterInput] = useState();
-  const [text, setText] = useState();
-
-  function HandleInput(e) {
-    setText(e.target.value);
-  }
-
-  function HandleOnClick() {
-    setEnterInput(text);
-  }
-
+const App = () => {
   let Numbers = [
-    [1, 2, 3, "+"],
-    [4, 5, 6, "-"],
-    [7, 8, 9, "x"],
-    [0, ".", "=", "/"],
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [9, 0, "+", "-"],
+    ["X", "/", "%", "="]
   ];
 
   return (
     <>
-      <div className="Input" value={text} onChange={HandleInput()}>
-        <input type="text" />
-      </div>
-      <div className="Main">
-        {Numbers.map((Number1, index1) => (
-          <div key={index1} className="Content">
-            <ul>
-              {Number1.map((EnterInput, index2) => (
-                <li key={index2} onClick={HandleOnClick}>
-                  {EnterInput}
-                </li>
-              ))}
-            </ul>
-          </div>
+      
+      <h1 className="Content">
+      <div className="Input"><input type="text" /></div>
+        {Numbers.map((Elements, index) => (
+          <ul key={index}>
+            {Elements.map((Indices, index2) => (
+              <li>{Indices}</li>
+            ))}
+          </ul>
         ))}
-      </div>
+      </h1>
     </>
   );
-}
+};
 
 export default App;
