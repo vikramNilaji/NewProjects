bd860916a9a4a2a8bc18790ae64e37fde4ba9182
Vikram Nilaji <97115025+vikramNilaji@users.noreply.github.com>
2025-02-06 00:46:14 +0530

    add

--- Calculator1/src/App.jsx
+++ Calculator1/src/App.jsx
@@ -6,23 +6,27 @@ const App = () => {
     [1, 2, 3, 4],
     [5, 6, 7, 8],
     [9, 0, "+", "-"],
-    ["X", "/", "%", "="]
+    ["X", "/", "%", "="],
   ];
 
-   const [text,setText]=useState("");
+  const [text, setText] = useState("");
   function handleInput(textContent) {
     setText((prevText) => prevText + textContent);
+    console.log(text)
   }
 
   return (
     <>
-      
       <h1 className="Content">
-      <div className="Input"><input type="text" value={text} placeholder="type here" /></div>
+        <div className="Input">
+          <input type="text" value={text} placeholder="type here" />
+        </div>
         {Numbers.map((Elements, index) => (
           <ul key={index}>
             {Elements.map((Indices, index2) => (
-              <li key={index2} onClick={() => handleInput(Indices.toString())}>{Indices}</li>
+              <li key={index2} onClick={() => handleInput(Indices.toString())}>
+                {Indices}
+              </li>
             ))}
           </ul>
         ))}
