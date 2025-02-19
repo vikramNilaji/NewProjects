a0764155b02801aa837d81356b24e7285ac79597
Vikram Nilaji <97115025+vikramNilaji@users.noreply.github.com>
2025-02-19 08:20:52 +0530

    add

--- Corosal/src/App.jsx
+++ Corosal/src/App.jsx
@@ -1,11 +1,8 @@
+import { useEffect, useState } from "react";
+import { BsArrowLeftCircleFill, BsArrowRightCircleFill } from "react-icons/bs";
+import "./styles.css";
 
-import { useEffect } from "react";
-import "./App.css";
-import { useState } from "react";
-
-const  App=({url} ,limit = 5, page = 2) =>{
-  const [count, setCount] = useState(0);
-
+export default function App({ url, limit = 5, page = 1 }) {
   const [images, setImages] = useState([]);
   const [currentSlide, setCurrentSlide] = useState(0);
   const [errorMsg, setErrorMsg] = useState(null);
@@ -13,9 +10,9 @@ const  App=({url} ,limit = 5, page = 2) =>{
 
   async function fetchImages(getUrl) {
     try {
-      const response = await fetch(
-        ` ${getUrl} ? page=${page} & limit=${limit}`
-      );
+      setLoading(true);
+
+      const response = await fetch(`${getUrl}?page=${page}&limit=${limit}`);
       const data = await response.json();
 
       if (data) {
@@ -28,36 +25,67 @@ const  App=({url} ,limit = 5, page = 2) =>{
     }
   }
 
+  function handlePrevious() {
+    setCurrentSlide(currentSlide === 0 ? images.length - 1 : currentSlide - 1);
+  }
 
-useEffect(
-  () => {
+  function handleNext() {
+    setCurrentSlide(currentSlide === images.length - 1 ? 0 : currentSlide + 1);
+  }
+
+  useEffect(() => {
     if (url !== "") fetchImages(url);
-  },
-  [url]
-);
+  }, [url]);
 
-console.log(images);
+  console.log(images);
 
-if (loading) {
-  <div>Loading data ! Pleas wait</div>;
+  if (loading) {
+    return <div>Loading data ! Please wait</div>;
+  }
+
+  if (errorMsg !== null) {
+    return <div>Error occured ! {errorMsg}</div>;
+  }
+
+  return (
+    <div className="container">
+      <BsArrowLeftCircleFill
+        onClick={handlePrevious}
+        className="arrow arrow-left"
+      />
+      {images && images.length
+        ? images.map((imageItem, index) => (
+            <img
+              key={imageItem.id}
+              alt={imageItem.download_url}
+              src={imageItem.download_url}
+              className={
+                currentSlide === index
+                  ? "current-image"
+                  : "current-image hide-current-image"
+              }
+            />
+          ))
+        : null}
+      <BsArrowRightCircleFill
+        onClick={handleNext}
+        className="arrow arrow-right"
+      />
+      <span className="circle-indicators">
+        {images && images.length
+          ? images.map((_, index) => (
+              <button
+                key={index}
+                className={
+                  currentSlide === index
+                    ? "current-indicator"
+                    : "current-indicator inactive-indicator"
+                }
+                onClick={() => setCurrentSlide(index)}
+              ></button>
+            ))
+          : null}
+      </span>
+    </div>
+  );
 }
-
-if (errorMsg === null) {
-  <div>
-    error occurred ! {errorMsg}
-  </div>;
-}
-
- <div className="container"></div>;
-
-}
-
-
-  return ( <>
-          
-
-  </>
-)
-
-
-export default App;
--- Corosal/src/main.jsx
+++ Corosal/src/main.jsx
@@ -4,6 +4,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <>
-    <App url={'https://placekittens.com/g/200/300'} limit={4} page={1} />
+    <App url={'https://picsum.photos/v2/list'} page={"1"} limit={'4'}/>
   </>,
 )
+ 
