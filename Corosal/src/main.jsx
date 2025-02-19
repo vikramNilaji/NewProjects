ce7d1a8d4c2d2c3097b707dedb673f939edbef67
vikramNilaji <vik.nilaji@gmail.com>
2025-02-19 16:51:55 +0530

    add

--- Corosal/src/main.jsx
+++ Corosal/src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <>
-    <App url={'https://picsum.photos/v2/list'} page={"1"} limit={'4'}/>
+    <App url={'https://picsum.photos/v2/list'} page={"1"} limit={'10'}/>
   </>,
 )
  
