402ee4e29f31e60f4ee4a60b25682aff7302642d
Vikram Nilaji <97115025+vikramNilaji@users.noreply.github.com>
2025-02-26 23:47:16 +0530

    add

--- APILearning/src/App.jsx
+++ APILearning/src/App.jsx
@@ -1,44 +1,44 @@
-import React,{ useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 
-const TryCatch=()=>{
-
-  const [data,setData] =useState(null);
-  const [error,setError]= useState(null)
-  const fetchData= async()=>{
+const TryCatch = () => {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const fetchData = async () => {
     try {
-      const response = await fetch("https://jsonplaceholder.typicode.com/users");
-      if(!response.ok) {
-        throw new Error(`HTTP error !  Status: ${response.status}`)
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+      if (!response.ok) {
+        throw new Error(`HTTP error !  Status: ${response.status}`);
+      } else {
+        const result = await response.json();
+        setData(result);
       }
-      else {
-      const result = await response.json();
-      setData(result);
-      }
+    } catch (e) {
+      setError(e.message);
     }
-      catch (e) {
-        setError (e.message)
-      }
   };
 
-  useEffect(()=>{
+  useEffect(() => {
     fetchData();
-  },[])
+  }, []);
   return (
     <div>
-    <h2> User List</h2>
-    {error ? ( <h3 style={ {color: "red"}} > Error :{error} </h3> ) 
-    : data ?
-    ( <ul> {data.map((user)=>( <li key={user.id}>{user.name}</li>))} </ul> ) :
-    ( <p> Loading...</p>
-    
-    )}
-
-
+      <h2> User List</h2>
+      {error ? (
+        <h3 style={{ color: "red" }}> Error :{error} </h3>
+      ) : data ? (
+        <ul>
+          {" "}
+          {data.map((user) => (
+            <li key={user.id}>{user.name}</li>
+          ))}{" "}
+        </ul>
+      ) : (
+        <p> Loading...</p>
+      )}
     </div>
-  )
-  
-}
+  );
+};
 
-
-export default TryCatch
-
+export default TryCatch;
