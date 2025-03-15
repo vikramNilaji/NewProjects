5dba205bba2a33c9cf5683001cbbbe83fa331c4a
Vikram Nilaji <97115025+vikramNilaji@users.noreply.github.com>
2025-03-15 22:05:12 +0530

    add

--- RandomColorsProject/src/App.jsx
+++ RandomColorsProject/src/App.jsx
@@ -1,24 +1,25 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-   
-  function RandomColorCode(length){
-    return Math.floor(Math.random()*length)
+  function RandomColorCode(length) {
+    return Math.floor(Math.random() * length);
   }
 
-  // for(let i=2;i<21;i+2){
-
-  //   console.log(i)
   // }
+  let num = [3000, 5, 2, 3491, 342, 900023, 232, 232, 3000, 2, 5];
+  let a = 3000;
+  let b = [];
+  for (let i = 0; i < num.length; i++) {
+    if (num[i] === a) {
+      console.log(num[i]);
+      b.push(num[i]);
+      console.log(b);
+      console.log(b.length);
+    }
+  }
 
-
-  
-  return (
-    <>
-        
-    </>
-  )
+  return <></>;
 }
 
-export default App
+export default App;
