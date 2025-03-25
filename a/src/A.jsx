5a4e3335d06a9c8bbb84b45426c752b1a97883e4
Vikram Nilaji <97115025+vikramNilaji@users.noreply.github.com>
2025-03-25 19:58:13 +0530

    add

--- a/src/A.jsx
+++ a/src/A.jsx
@@ -2,23 +2,21 @@ import React, { useState } from "react";
 import { useEffect } from "react";
 
 const A = () => {
+  const [count, setCount] = useState(0);
+  const [name, setName] = useState("vikram");
 
-  const[count,setCount]=useState(0)
-  const[name,setName]=useState('vikram')
-
-  function ChangeSet(){
-    setCount(count+1)
-    setName("Pooja")
+  function ChangeSet() {
+    setCount(count + 1);
+    setName("Pooja");
   }
   useEffect(() => {
     console.log(`${name} Welcome to useEffect world  ${count}`);
-  }, [count,name]);
-
+  }, []);
 
   return (
     <div>
-           <button onClick={ChangeSet}> Click</button>
-           {console.log(' Good Morning ')}
+      <button onClick={ChangeSet}> Click</button>
+      {console.log("Good Morning")}
     </div>
   );
 };
