d69a6700295990b25f02d7f0781c2a5eca6c5bc3
vikramNilaji <vik.nilaji@gmail.com>
2025-03-26 10:51:05 +0530

    add

--- a/src/main.jsx
+++ a/src/main.jsx
@@ -2,11 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import A from './a.jsx'
+import A from './A.jsx'
+import C from "./C"
 
 createRoot(document.getElementById('root')).render(
   <>
   <A/>
+  <C/>
     <App />
   </>,
 )
