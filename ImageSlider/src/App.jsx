d583835bd5c13acb947178e0383aabc4bbe68af0
Vikram Nilaji <97115025+vikramNilaji@users.noreply.github.com>
2025-03-28 08:20:32 +0530

    add

--- ImageSlider/src/App.jsx
+++ ImageSlider/src/App.jsx
@@ -1,20 +1,26 @@
 import "./App.css";
 import ImageSlider from "./ImageSlider";
 // import ImageSlider1 from "./ImageSlider1"
+import ImageSlider3 from "./ImageSlider3";
 
 function App() {
   return (
-    <>
-      <ImageSlider
-        url={"https://picsum.photos/v2/list/"}
+     <>
+      {/* <ImageSlider
+       url={"https://picsum.photos/v2/list/"}
         page={"1"}
-        limit={"10"}
-      />  
-{/* 
+    limit={"10"}
+      /> */}
+
+      
+      {/* 
       <ImageSlider1  url={"https://picsum.photos/v2/list/"}
         page={"1"}
         limit={"10"}/> */}
+
       
+      
+      <ImageSlider3 url={"https://picsum.photos/v2/list/"} page={"1"} limit={"10"}/>
     </>
   );
 }
---
+++ ImageSlider/src/ImageSlider3.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
+
+const ImageSlider3 = ({ url, limit=5, page=1 }) => {
+  const [image, setImage] = useState([]);
+  const [errorMsg, setErrorMsg] = useState(null);
+  const [Loading,setLoading]=useState(false)
+  const FetchData = async (getUrl) => {
+    try {
+      setLoading(true)
+      const response = await fetch(`${getUrl}?page=${page}&limit=${limit}`);
+      const JsonData = await response.json();
+
+      if(JsonData){
+        setLoading(false)
+        setImage(JsonData)
+      }
+    
+    } catch (e) {
+      setLoading(false)
+      setErrorMsg(e.message);
+    }
+  };
+
+  console.log(image)
+
+  useEffect(()=>{
+    FetchData(url)
+  },[url])
+
+  if(errorMsg){
+    return (
+    <h1>Error Occured :  {errorMsg} </h1>
+    )
+  }
+
+ else if(Loading){
+    return (<h1> Loading... Please wait..</h1>  )
+  }
+
+  else{
+    return <div>
+    {image && image.length ? image.map((ImageItem,index)=>{return( <img src={ImageItem.download_url} alt={ImageItem.download_url} key={ImageItem.id} />)}): null}
+
+
+    </div>;
+
+  }
+  
+};
+
+export default ImageSlider3;
