30246eb557d6e25db414e45dd86558920782ca94
vikramNilaji <vik.nilaji@gmail.com>
2025-03-29 11:35:36 +0530

    add

--- ImageSlider/src/App.jsx
+++ ImageSlider/src/App.jsx
@@ -1,43 +1,28 @@
 import "./App.css";
 import ImageSlider from "./ImageSlider";
-<<<<<<< Updated upstream
+
 // import ImageSlider1 from "./ImageSlider1"
 import ImageSlider3 from "./ImageSlider3";
 
 function App() {
   return (
-     <>
+    <>
       {/* <ImageSlider
        url={"https://picsum.photos/v2/list/"}
         page={"1"}
     limit={"10"}
       /> */}
 
-      
       {/* 
       <ImageSlider1  url={"https://picsum.photos/v2/list/"}
         page={"1"}
         limit={"10"}/> */}
 
-=======
-import ImageSlider1 from "./ImageSlider1"
-
-function App() {
-  return (
-    <>
-       {/* <ImageSlider
+      <ImageSlider3
         url={"https://picsum.photos/v2/list/"}
         page={"1"}
         limit={"10"}
-      />    */}
-
-      <ImageSlider1  url={"https://picsum.photos/v2/list/"}
-        page={"1"}
-        limit={"10"}/> 
->>>>>>> Stashed changes
-      
-      
-      <ImageSlider3 url={"https://picsum.photos/v2/list/"} page={"1"} limit={"10"}/>
+      />
     </>
   );
 }
--- ImageSlider/src/ImageSlider3.jsx
+++ ImageSlider/src/ImageSlider3.jsx
@@ -1,53 +1,88 @@
 import React from "react";
 import { useState } from "react";
 import { useEffect } from "react";
+import "./App3.css"
+import {BsArrowLeftCircleFill,BsArrowRightCircleFill} from "react-icons/bs"
 
-const ImageSlider3 = ({ url, limit=5, page=1 }) => {
+const ImageSlider3 = ({ url, limit = 5, page = 1 }) => {
   const [image, setImage] = useState([]);
   const [errorMsg, setErrorMsg] = useState(null);
-  const [Loading,setLoading]=useState(false)
+  const [Loading, setLoading] = useState(false);
+  const [currentSlide,setCurrentSlide]=useState(0)
   const FetchData = async (getUrl) => {
     try {
-      setLoading(true)
+      setLoading(true);
       const response = await fetch(`${getUrl}?page=${page}&limit=${limit}`);
       const JsonData = await response.json();
 
-      if(JsonData){
-        setLoading(false)
-        setImage(JsonData)
+      if (JsonData) {
+        setLoading(false);
+        setImage(JsonData);
       }
-    
     } catch (e) {
-      setLoading(false)
+      setLoading(false);
       setErrorMsg(e.message);
     }
   };
 
-  console.log(image)
+  console.log(image);
 
-  useEffect(()=>{
-    FetchData(url)
-  },[url])
+  function handlePrevious(){
 
-  if(errorMsg){
+    setCurrentSlide(currentSlide=== 0 ? image.length-1 : currentSlide-1)
+
+
+  }
+
+  function handleNext(){
+    setCurrentSlide(currentSlide=== image.length-1? 0 : currentSlide+1)
+
+
+  }
+
+  useEffect(() => {
+    FetchData(url);
+  }, [url]);
+
+  if (errorMsg) {
+    return <h1>Error Occured : {errorMsg} </h1>;
+  } else if (Loading) {
+    return <h1> Loading... Please wait..</h1>;
+  } else {
     return (
-    <h1>Error Occured :  {errorMsg} </h1>
-    )
+      <div className="container1">
+        <h1>HI</h1>    {image && image.length
+          ? image.map((ImageItem, index) => {
+              return (
+                <img
+                  src={ImageItem.download_url}
+                  alt={ImageItem.download_url}
+                  key={ImageItem.id}
+                  className={currentSlide === index ? 'current-slide1' : '  hide-current-slide1' }
+                />
+              );
+            })
+          : null}
+
+<h1><BsArrowLeftCircleFill onClick={handlePrevious}  className="arrow arrow-left1"/></h1>
+<h1><BsArrowRightCircleFill onClick={handleNext}  className="arrow arrow-right1"/></h1>
+
+{image.length ? 
+  image.map((_, index) => (
+    <button key={index}
+    className={
+      currentSlide === index
+        ? "current-indicator1"
+        : "current-indicator1 inactive-indicator1"
+    } onClick={() => setCurrentSlide(index)}>
+    
+    </button>
+  )) 
+: null}
+  
+      </div>
+    );
   }
-
- else if(Loading){
-    return (<h1> Loading... Please wait..</h1>  )
-  }
-
-  else{
-    return <div>
-    {image && image.length ? image.map((ImageItem,index)=>{return( <img src={ImageItem.download_url} alt={ImageItem.download_url} key={ImageItem.id} />)}): null}
-
-
-    </div>;
-
-  }
-   
 };
 
 export default ImageSlider3;
