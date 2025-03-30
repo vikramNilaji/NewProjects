b9470bfbd3679329f292c774b35acd367b4a9f97
Vikram Nilaji <97115025+vikramNilaji@users.noreply.github.com>
2025-03-30 19:44:55 +0530

    add

--- a/src/App.jsx
+++ a/src/App.jsx
@@ -1,23 +1,23 @@
-import { useState } from 'react'
+import { useState } from "react";
 import { FaStar } from "react-icons/fa";
 
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [color, setColor] = useState("red")
+  const [color, setColor] = useState("red");
 
   return (
     <>
-      {[...Array(5)].map((_,index)=>{
-        return(
-          <div key={index} className='StarColor'>   <FaStar> </FaStar></div>
-       
-          
-
-        )
+      {[...Array(5)].map((_, index) => {
+        return (
+          <div key={index} className="StarColor">
+            {" "}
+            <FaStar> </FaStar>
+          </div>
+        );
       })}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
