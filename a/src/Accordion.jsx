69a101c0f37232585a87302471db9cb575bf0d83
Vikram Nilaji <97115025+vikramNilaji@users.noreply.github.com>
2025-03-31 10:29:29 +0530

    add

--- a/src/Accordion.jsx
+++ a/src/Accordion.jsx
@@ -1,54 +1,14 @@
-import React, { useState } from "react";
-import Data from "./Data.js";
+import React from "react";
+
+import { useState } from "react";
 
 const Accordion = () => {
-  const [selected, setSelected] = useState([]);
-  const [enableMultiSelection, setEnableMultiSelection] = useState(true);
 
-  function SingleHandleClick(currentId) {
-    setSelected(selected.includes(currentId) ? [] : [currentId]);
-  }
 
-  function MultiHandleClick(currentId) {
-    if (selected.includes(currentId)) {
-      setSelected(selected.filter((id) => id));
-    } else {
-      setSelected([...selected, currentId]);
-    }
-  }
+  return <div>
 
-  function ToggleMultiSelectionButton() {
-    setEnableMultiSelection(!enableMultiSelection);
-  }
-  return (
-    <>
-      <div>
-        <button onClick={ToggleMultiSelectionButton}>
-          {" "}
-          {enableMultiSelection ? "MultiSlection" : "SingleSelection"}
-        </button>
-        {Data.map((items, id) => {
-          return (
-            <div key={id}>
-              <h2>{items.id}</h2>
-              <h2>{items.question}</h2>
-              <h2
-                onClick={
-                  enableMultiSelection
-                    ? () => SingleHandleClick(items.id)
-                    : () => MultiHandleClick(items.id)
-                }
-              >
-                {" "}
-                +{" "}
-              </h2>
-              {selected.includes(items.id) ? <h2> {items.answer}</h2> : []}
-            </div>
-          );
-        })}
-      </div>
-    </>
-  );
+
+  </div>;
 };
 
 export default Accordion;
