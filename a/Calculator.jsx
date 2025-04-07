c3a5ed2ee1efc72160e27dbf833dfaeeb48c4fd1
Vikram Nilaji <97115025+vikramNilaji@users.noreply.github.com>
2025-04-07 08:08:32 +0530

    add

--- a/Calculator.jsx
+++ a/Calculator.jsx
@@ -3,6 +3,7 @@ import { evaluate } from "mathjs";
 const Calculator = () => {
   const Numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, "+", "-", "/", "C", "="];
   const [text, setText] = useState("");
+  const [evaluated, setEvaluated] = useState(true);
 
   function EnterText(event) {
     const newValue = event.target.innerHTML;
@@ -10,30 +11,33 @@ const Calculator = () => {
     if (newValue === "=") {
       try {
         setText(evaluate(text).toString());
+        setEvaluated(false);
       } catch {
         setText("error");
+        setEvaluated(true);
       }
     } else if (newValue === "C") {
       setText("");
     } else {
-      setText((prev) => prev + newValue);
+      if (evaluated === false) {
+        setText(newValue);
+      } else {
+        setText((prev) => prev + newValue);
+        setEvaluated(true);
+      }
     }
   }
 
   return (
     <div>
-      <input type="text" value={text}  />
+      <input type="text" value={text} />
       {Numbers.map((item, index) => (
         <button onClick={EnterText} key={index}>
           {item}
         </button>
       ))}
     </div>
-
- 
-    )
-
-
-  }
+  );
+};
 
 export default Calculator;
