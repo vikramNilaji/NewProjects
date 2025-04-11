97dfbe6e7003a821eb71988cc2c61c3c84c3ecbc
Vikram Nilaji <97115025+vikramNilaji@users.noreply.github.com>
2025-04-11 23:35:42 +0530

    add

--- vite-project/src/LoadMoreData.jsx
+++ vite-project/src/LoadMoreData.jsx
@@ -8,16 +8,16 @@ const LoadMoreData = () => {
 
   const FetchProducts = async () => {
     try {
-      setLoading(true)
+      setLoading(true);
       let response = await fetch(
         `https://dummyjson.com/products?limit=20&skip=${
           count === 0 ? 0 : count * 20
         }`
       );
       const data1 = await response.json();
-      if(result&&result.products&&result.products.length){
-        setProducts(data1.products)
-        setLoading(true)
+      if (data1 && data1.products && data1.products.length) {
+        setProducts(data1.products);
+        setLoading(true);
       }
 
       console.log(data1);
@@ -30,7 +30,31 @@ const LoadMoreData = () => {
     FetchProducts();
   }, []);
 
-  return <div className="container">LoadMoreData</div>;
+  if (loading) {
+    return <div> Items loading... Please wait....</div>;
+  } else {
+    return (
+      <div className="container">
+        <div className="product-container">
+          {products && products.length
+            ? products.map((item) => {
+                return (
+                  <div className="products" key={item.id}>
+                    {" "}
+                    <img src={item.thumbnail} alt={item.title} /> {item.title}{" "}
+                  </div>
+                );
+              })
+            : null}
+        </div>
+
+        <div>
+
+          <button className="button-container">Load More Products</button>
+        </div>
+      </div>
+    );
+  }
 };
 
 export default LoadMoreData;
