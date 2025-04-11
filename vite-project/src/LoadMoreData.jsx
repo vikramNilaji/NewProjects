72e456b0206379a881150cdef1a7323bc407fc06
Vikram Nilaji <97115025+vikramNilaji@users.noreply.github.com>
2025-04-11 22:49:56 +0530

    add

--- vite-project/src/LoadMoreData.jsx
+++ vite-project/src/LoadMoreData.jsx
@@ -1,29 +1,36 @@
-import React from 'react'
-import { useState } from 'react'
+import React, { useEffect } from "react";
+import { useState } from "react";
 
 const LoadMoreData = () => {
-    const [products,setProducts]=useState([])
-    const [loading,setLoading]=useState(false)
-    const [count,setCount]=useState(0)
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [count, setCount] = useState(0);
 
-    async function FetchProducts(){
+  const FetchProducts = async () => {
+    try {
+      setLoading(true)
+      let response = await fetch(
+        `https://dummyjson.com/products?limit=20&skip=${
+          count === 0 ? 0 : count * 20
+        }`
+      );
+      const data1 = await response.json();
+      if(result&&result.products&&result.products.length){
+        setProducts(data1.products)
+        setLoading(true)
+      }
 
+      console.log(data1);
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    const FetchProducts=async()=>{
+  useEffect(() => {
+    FetchProducts();
+  }, []);
 
-        try {
-            
-        } catch (error) {
-            console.log(error)
-        }
+  return <div className="container">LoadMoreData</div>;
+};
 
-    }
-
-
-  return (
-    <div className='container'>LoadMoreData</div>
-  )
-}
-
-export default LoadMoreData
+export default LoadMoreData;
