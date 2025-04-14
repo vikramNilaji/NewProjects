ce2725bd4bca4775352d0a5eb49ac7c043d4598b
Vikram Nilaji <97115025+vikramNilaji@users.noreply.github.com>
2025-04-14 14:27:21 +0530

    add

--- vite-project/src/LoadMoreData.jsx
+++ vite-project/src/LoadMoreData.jsx
@@ -1,59 +1,58 @@
-import React, { useEffect } from "react";
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 const LoadMoreData = () => {
-  const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(false);
   const [count, setCount] = useState(0);
+  const [products, setProducts] = useState([]);
 
-  const FetchProducts = async () => {
+  async function FetchData() {
     try {
       setLoading(true);
-      let response = await fetch(
-        `https://dummyjson.com/products?limit=20&skip=${
-          count === 0 ? 0 : count * 20
-        }`
+      const response = await fetch(
+        `https://dummyjson.com/products?limit=20&skip=${count * 20}`
       );
-      const data1 = await response.json();
-      if (data1 && data1.products && data1.products.length) {
-        setProducts(data1.products);
-        setLoading(true);
-      }
+      const result = await response.json();
 
-      console.log(data1);
+      if (result && result.products && result.products.length) {
+        setProducts((prev) => [...prev, ...result.products]);
+        setCount((prev) => prev + 1); // increment page count
+      }
+      setLoading(false);
+
+      console.log(result);
     } catch (error) {
-      console.log(error);
+      setLoading(false);
+      console.log(error.message);
     }
-  };
+  }
 
   useEffect(() => {
-    FetchProducts();
+    FetchData();
   }, []);
 
-  if (loading) {
-    return <div> Items loading... Please wait....</div>;
-  } else {
-    return (
-      <div className="container">
-        <div className="product-container">
-          {products && products.length
-            ? products.map((item) => {
-                return (
-                  <div className="products" key={item.id}>
-                    {" "}
-                    <img src={item.thumbnail} alt={item.title} /> {item.title}{" "}
-                  </div>
-                );
-              })
-            : null}
-        </div>
-
-        <div>
-          <button className="button-container">Load More Products</button>
-        </div>
+  return (
+    <div className="container">
+      <div className="product-container">
+        {products.map((item,index) => (
+          <div className="product" key={item.index}>
+            <img src={item.images[0]} alt={item.brand} />
+            <p>{item.title}</p>
+          </div>
+        ))}
       </div>
-    );
-  }
+
+      <div>
+        <button
+          className="ButtonContainer"
+          onClick={FetchData}
+          disabled={loading}
+        >
+          {loading ? "Loading..." : "Load More Data"}
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default LoadMoreData;
