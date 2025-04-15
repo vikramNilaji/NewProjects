3f33386202e47d4f4517a094894d6c4d1ec7de1f
vikramNilaji <vik.nilaji@gmail.com>
2025-04-15 16:19:47 +0530

    add

--- a/src/Calculator11.jsx
+++ a/src/Calculator11.jsx
@@ -1,26 +1,56 @@
-import React from 'react'
-import './Cal.css'
-import { useState } from 'react'
+import React from "react";
+import "./Cal.css";
+import { useState } from "react";
+import { evaluate } from "mathjs";
 
 const Calculator11 = () => {
-  const[text,setText]=useState("")
-   
-  function ChangeHandler(e){
-    setText(e.target.value)
+  const [text, setText] = useState("");
+
+  function ChangeHandler(NumItems) {
+    if (NumItems === "=") {
+      setText(evaluate(text));
+    } else if (NumItems === "C") {
+      setText("");
+    } else {
+      setText((prevValue) => prevValue + NumItems);
+    }
   }
 
-  let Letters=[[1,2,3,4],[5,6,7,8],[9,0,'+','-'],['*','/','C','=']]
+  //  function TextHandler(e){
+
+  //     setText(e.target.value)
+  //   }
+
+  let Letters = [
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [9, 0, "+", "-"],
+    ["*", "/", "C", "="],
+  ];
 
   return (
-    <div className='Box'>
-    <input type="text" value={text} onChange={ChangeHandler}/>
+    <div className="Box">
+      <input type="text" value={text} />
 
-     {Letters.map((items)=>{return(<div>{items.map((semiItems)=>{return(<div>{semiItems}</div>)})}</div>)})}
-
-
-      
+      {Letters.map((items, index) => {
+        return (
+          <div key={index} className="NumContainer">
+            {items.map((semiItems, index1) => {
+              return (
+                <div
+                  key={index1}
+                  className="Numbers"
+                  onClick={() => ChangeHandler(semiItems)}
+                >
+                  {semiItems}
+                </div>
+              );
+            })}
+          </div>
+        );
+      })}
     </div>
-  )
-}
+  );
+};
 
-export default Calculator11
+export default Calculator11;
