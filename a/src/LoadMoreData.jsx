e6628f7175a9bfe6e4fc0d88b57263317c2eac86
Vikram Nilaji <97115025+vikramNilaji@users.noreply.github.com>
2025-04-25 04:01:29 +0530

    add

--- a/src/LoadMoreData.jsx
+++ a/src/LoadMoreData.jsx
@@ -1,22 +1,25 @@
 import { useState } from "react";
 import { useEffect } from "react";
+import "./App7.css"
 
 const LoadMoreData = () => {
-  const [products, setProducts] = useState(null);
+  const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(false);
   const [count, setCount] = useState(0);
+  const [disableItems,setDisableItems]=useState(false)
 
   const FetchData = async () => {
     try {
       setLoading(true);
-
       const response = await fetch(
-        "https://picsum.photos/v2/list?page=1&limit=5"
+        `https://dummyjson.com/products?limit=20&skip=${
+          count === 0 ? 0 : count * 20
+        }`
       );
       const ImageData = await response.json();
-      if (ImageData && ImageData.length) {
+      if (ImageData && ImageData.products && ImageData.products.length) {
         setLoading(false);
-        setProducts(ImageData);
+        setProducts((prevData)=>[...prevData, ...ImageData.products]);
       }
       console.log(ImageData);
     } catch (error) {
@@ -26,29 +29,39 @@ const LoadMoreData = () => {
 
   useEffect(() => {
     FetchData();
-  }, []);
+  }, [count]);
+
+  useEffect(()=>{
+    if(products && products.length === 100 ){
+      setDisableItems(true)
+    }
+
+  },[products])
 
   if (loading) {
     return <div> Loading Data... Please wait..</div>;
   } else {
     return (
-      <div className="container">
-        <div>
+      <div className="load-more-container"> 
+        <div className="product-container">
           {products && products.length
             ? products.map((items, index) => {
                 return (
-                  <img
-                    key={index}
-                    src={items.download_url}
-                    alt={items.download_url}
-                  />
+                  <img className="product" key={items.id} src={items.thumbnail} alt={items.title} />
                 );
               })
             : null}
+        </div>
+        <div className="button-container">
+        <button onClick={() => setCount(count + 1)} disabled={disableItems}>
+  Load More Products
+</button>
+
+{disableItems ? <div> You have reached to maximum items</div> : null}
         </div>
       </div>
     );
   }
 };
-
+ 
 export default LoadMoreData;
