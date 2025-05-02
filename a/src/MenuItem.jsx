8f1fc9330ea33caa4ce2a17ced8ddd8ba208c674
vikramNilaji <vik.nilaji@gmail.com>
2025-05-02 16:42:08 +0530

    add

--- a/src/MenuItem.jsx
+++ a/src/MenuItem.jsx
@@ -3,36 +3,36 @@ import MenuList from "./MenuList";
 import { useState } from "react";
 import { FaMinus, FaPlus } from "react-icons/fa";
 
-// const MenuItem = ({ item }) => {
-//   const [displayCurrentChildren, setDisplayCurrentChildren] = useState({});
+const MenuItem = ({ item }) => {
+  const [displayCurrentChildren, setDisplayCurrentChildren] = useState({});
 
-//   function handleToggleChildren(getCurrentLabel) {
-//     setDisplayCurrentChildren({
-//       ...displayCurrentChildren,
-//       [getCurrentLabel]: !displayCurrentChildren[getCurrentLabel],
-//     });
-//   }
+  function handleToggleChildren(getCurrentLabel) {
+    setDisplayCurrentChildren({
+      ...displayCurrentChildren,
+      [getCurrentLabel]: !displayCurrentChildren[getCurrentLabel],
+    });
+  }
 
-//   console.log(displayCurrentChildren)
+  console.log(displayCurrentChildren)
 
-//   return (
-//     <li>
-//       <div style={{ display: "flex", gap: "20px" }}>
-//         <p>{item.label}</p>
-//         {item && item.children && item.children.length  ? (
-//           <span onClick={() => handleToggleChildren(item.label)}> {displayCurrentChildren[item.label] ? <FaMinus/> : <FaPlus/> } </span>
-//         ) : null}
-//       </div>
+  return (
+    <li>
+      <div style={{ display: "flex", gap: "20px" }}>
+        <p>{item.label}</p>
+        {item && item.children && item.children.length  ? (
+          <span onClick={() => handleToggleChildren(item.label)}> {displayCurrentChildren[item.label] ? <FaMinus/> : <FaPlus/> } </span>
+        ) : null}
+      </div>
 
-//       {item && item.children && item.children.length > 0 && displayCurrentChildren[item.label]? (
-//         <MenuList list={item.children} />
-//       ) : null}
-//     </li>
-//   );
-// };
-
-const MenuItem = ({item}) => {
-  return <li> {item.label} </li>;
+      {item && item.children && item.children.length > 0 && displayCurrentChildren[item.label]? (
+        <MenuList list={item.children} />
+      ) : null}
+    </li>
+  );
 };
+
+// const MenuItem = ({item}) => {
+//   return <li> {item.label} </li>;
+// };
 
 export default MenuItem;
