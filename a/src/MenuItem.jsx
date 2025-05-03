c1d3b54a60ff9b8906488bb9222a3a23d2d2e4b5
vikramNilaji <vik.nilaji@gmail.com>
2025-05-03 11:41:10 +0530

    add

--- a/src/MenuItem.jsx
+++ a/src/MenuItem.jsx
@@ -13,18 +13,24 @@ const MenuItem = ({ item }) => {
     });
   }
 
-  console.log(displayCurrentChildren)
+  console.log(displayCurrentChildren);
 
   return (
     <li>
       <div style={{ display: "flex", gap: "20px" }}>
         <p>{item.label}</p>
-        {item && item.children && item.children.length  ? (
-          <span onClick={() => handleToggleChildren(item.label)}> {displayCurrentChildren[item.label] ? <FaMinus/> : <FaPlus/> } </span>
+        {item && item.children && item.children.length ? (
+          <span onClick={() => handleToggleChildren(item.label)}>
+            {" "}
+            {displayCurrentChildren[item.label] ? <FaMinus /> : <FaPlus />}{" "}
+          </span>
         ) : null}
       </div>
 
-      {item && item.children && item.children.length > 0 && displayCurrentChildren[item.label]? (
+      {item &&
+      item.children &&
+      item.children.length > 0 &&
+      displayCurrentChildren[item.label] ? (
         <MenuList list={item.children} />
       ) : null}
     </li>
