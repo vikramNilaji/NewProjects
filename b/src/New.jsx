5a42b1f6030e6b6c554044a96090701dfd6de02d
vikramNilaji <vik.nilaji@gmail.com>
2025-05-16 16:45:39 +0530

    add

--- b/src/New.jsx
+++ b/src/New.jsx
@@ -1,21 +1,47 @@
+import { useState } from "react";
+
 const New = () => {
   const Data = [
-    { name: "Vikram", surname: "Nilaji" },
+    { name: "Vikram", surname: "Nilaji", children: [{ name: "Maanya" }] },
     { name: "Pooja", surname: "Nilaji", children: [{ name: "Maanya" }] },
-    { name: "Sumitra", surname: "Nilaji" },
+    { name: "Sumitra", surname: "Nilaji", children: [{ name: "Pintu" }] },
   ];
 
-  const UpdateData = Data.map((item, index) => {
-    if (item.name === "Pooja") {
-      return { ...item, Age: 29 };
+  const updatedData = Data.map((item, index) => {
+    if (item.children) {
+      return { ...item, Age: 23 };
     } else {
-      return item;
+      item;
     }
   });
 
-  console.log(UpdateData);
+  console.log(updatedData);
+  const [selectedData, setSelectedData] = useState({});
 
-  return <></>;
+  function HandlePlus(childkey) {
+    setSelectedData({ ...selectedData,[childkey]: !selectedData[childkey] });
+  }
+console.log(selectedData)
+  return (
+    <div>
+      {updatedData.map((item, index) => {
+        return (
+          <div key={index}>
+            {" "}
+            {item.name}
+            {item && item.children && item.children.length ? (
+              <span onClick={() => HandlePlus(item.name)}>{selectedData[item.name] ? "-": "+"}</span>
+            ) : null}
+            {item && item.children && item.children.length && selectedData[item.name]
+              ? item.children.map((child, index) => {
+                  return <div key={index}> {child.name} </div>;
+                })
+              : null}
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
-export default New ;
+export default New;
