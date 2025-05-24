16578ef6d35fa7a93593e4369c3ad5fa0eb5fade
Vikram Nilaji <97115025+vikramNilaji@users.noreply.github.com>
2025-05-24 18:32:00 +0530

    add

---
+++ Projects/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import CustomScroll from './CustomScroll'
+
+function App() {
+  
+
+  return (
+    <>
+     <CustomScroll url={'https://dummyjson.com/products?limit=100'}/>
+    </>
+  )
+}
+
+export default App
