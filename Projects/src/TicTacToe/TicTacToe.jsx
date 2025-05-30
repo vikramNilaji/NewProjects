4ae1247f68adc0d7d2c4e224df820dd6cf33d219
vikramNilaji <vik.nilaji@gmail.com>
2025-05-30 16:54:23 +0530

    add

--- Projects/src/TicTacToe/TicTacToe.jsx
+++ Projects/src/TicTacToe/TicTacToe.jsx
@@ -1,25 +1,40 @@
-import React from "react";
-
+import "./Tic.css";
+import { useState, useEffect } from "react";
+function Square({ value, onClick }) {
+  return (
+    <button onClick={onClick} className="square">
+      {value}
+    </button>
+  );
+}
 const TicTacToe = () => {
-  function Square({value}) {
-    return <button>{value}</button>;
+  const [squares, setSqaures] = useState(Array(9).fill(""));
+  const [isXTurn, setIsXTurn] = useState(true);
+  // console.log(squares);
+  function handleClick(getCurrentSquare) {
+    let copySquares = [...squares];
+    if(copySquares[getCurrentSquare]) return;
+    copySquares[getCurrentSquare] = isXTurn ? "X" : "O";
+    setIsXTurn(!isXTurn);
+    setSqaures(copySquares);
   }
+
   return (
     <div className="tictactoe-container">
       <div className="row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[0]} onClick={() => handleClick(0)} />
+        <Square value={squares[1]} onClick={() => handleClick(1)} />
+        <Square value={squares[2]} onClick={() => handleClick(2)} />
       </div>
       <div className="row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[3]} onClick={() => handleClick(3)} />
+        <Square value={squares[4]} onClick={() => handleClick(4)} />
+        <Square value={squares[5]} onClick={() => handleClick(5)} />
       </div>
       <div className="row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[6]} onClick={() => handleClick(6)} />
+        <Square value={squares[7]} onClick={() => handleClick(7)} />
+        <Square value={squares[8]} onClick={() => handleClick(8)} />
       </div>
     </div>
   );
