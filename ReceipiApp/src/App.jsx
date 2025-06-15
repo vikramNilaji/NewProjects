d6aab8f67e481fbb90b288c2eaf870b28bef8aea
Vikram Nilaji <97115025+vikramNilaji@users.noreply.github.com>
2025-06-15 18:54:49 +0530

    add

--- ReceipiApp/src/App.jsx
+++ ReceipiApp/src/App.jsx
@@ -2,9 +2,13 @@ import './App.css'
 
 function App() {
   return (
-    <>
-      
-    </>
+    <div className='text-red-500'>
+      Good Morning
+      <div className="p-4 md:p-8 lg:p-12 bg-gradient-to-r from-blue-500 to-purple-600 text-white">
+  Responsive Tailwind Box 📱💻🖥️
+</div>
+
+    </div>
   )
 }
 
