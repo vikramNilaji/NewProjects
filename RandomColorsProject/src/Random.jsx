65e447086093bb8c0b9b1e487b1e0c3acadab43d
vikramNilaji <vik.nilaji@gmail.com>
2025-07-08 16:49:52 +0530

    add

---
+++ RandomColorsProject/src/Random.jsx
@@ -0,0 +1,89 @@
+import React from "react";
+import "./Random.css";
+import { useState } from "react";
+
+const Random = () => {
+  const [color, setColor] = useState("#fffff")
+  const [value,setValue]=useState(true)
+  function HandleRandomHexNumbers(length) {
+    return Math.floor(Math.random() * length);
+  }
+
+  function HandleRGBNumber() {
+    return Math.floor(Math.random() * 255);
+  }
+  function RandomHexNumbers() {
+    let HexNumbers = [
+      1,
+      2,
+      3,
+      4,
+      5,
+      6,
+      7,
+      8,
+      9,
+      0,
+      "A",
+      "B",
+      "C",
+      "D",
+      "E",
+      "F",
+    ];
+    let Hex = "#";
+    for (let i = 0; i < 6; i++) {
+      Hex = Hex + HexNumbers[HandleRandomHexNumbers(HexNumbers.length)];
+      console.log(Hex);
+    }
+
+    return Hex;
+  }
+
+  function RandomRGBColor() {
+    let a = `rgb( ${HandleRGBNumber()},${HandleRGBNumber()},${HandleRGBNumber()})`;
+
+    return a;
+  }
+
+  function HexButton(){
+    setValue(true)
+  }
+
+  function RGBButton(){
+    setValue(false)
+  }
+
+
+  function ChangeFormat(){
+
+    if(value){
+      setColor(RandomHexNumbers())
+    }
+    else{
+      setColor(RandomRGBColor())
+    }
+
+  }
+
+  return (
+    <div className="container">
+      <div
+        className="Color"
+        style={{ backgroundColor: color, borderColor: "#abcdef" }}
+      >
+
+        {color}
+      </div>
+      <div className="ButtonContainer">
+        <button className="buttons" onClick={HexButton}>Hex Color Format</button>
+        <button className="buttons" onClick={RGBButton}>RGB Color Format</button>
+        <button className="buttons"  onClick={ChangeFormat}>
+          Change Color
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Random;
