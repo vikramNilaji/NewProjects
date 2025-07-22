20d432257412d327ce66ab25b516c5add64d6f9a
vikramNilaji <vik.nilaji@gmail.com>
2025-07-22 11:56:10 +0530

    add

--- Accordion/src/NewAccordion.jsx
+++ Accordion/src/NewAccordion.jsx
@@ -1,52 +1,10 @@
-import Data from "./Data.js";
-import { useState } from "react";
+import React from 'react'
+import Data from "./Data.js"
 
 const NewAccordion = () => {
-  const [selectedId, setSelectedId] = useState([]);
-  const [SingleSelection, setSingleSelection] = useState(true);
-
-  function HandleSingleClick(ItemId) {
-    setSelectedId(selectedId.includes(ItemId) ? [] : [ItemId]);
-    console.log(selectedId);
-  }
-
-  function HandleMultiClick(ItemId) {
-    setSelectedId(
-      selectedId.includes(ItemId)
-        ? (selectedId) => selectedId.filter((id) => id !== ItemId)
-        : (selectedId) => [...selectedId, ItemId]
-    );
-    console.log(selectedId)
-  }
-
   return (
-    <div>
-      <button onClick={() => setSingleSelection(!SingleSelection)}>
-        {SingleSelection ? "MultiSelection" : "SingleSelection"}{" "}
-      </button>
-      {Data.map((items, index) => {
-        return (
-          <div key={index}>
-            <div>{items.id}</div>
-            <div>{items.question}</div>
-            <div
-              onClick={
-                SingleSelection
-                  ? () => HandleSingleClick(items.id)
-                  : () => HandleMultiClick(items.id)
-              }
-            >
-              +
-            </div>
-            <div>
-              {" "}
-              {selectedId.includes(items.id) ? <div>{items.answer}</div> : null}
-            </div>
-          </div>
-        );
-      })}
-    </div>
-  );
-};
+    <div> {Data.map((items, index) => { return (<div>{items}</div>) })}  </div>
+  )
+}
 
-export default NewAccordion;
+export default NewAccordion
