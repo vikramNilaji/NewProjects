2582fc224c8f7ea64904ac85bb2ead884589bf36
vikramNilaji <vik.nilaji@gmail.com>
2025-07-22 14:07:52 +0530

    add

--- Accordion/src/NewAccordion.jsx
+++ Accordion/src/NewAccordion.jsx
@@ -1,10 +1,48 @@
-import React from 'react'
-import Data from "./Data.js"
+import React from "react";
+import Data from "./Data.js";
+import { useState } from "react";
 
 const NewAccordion = () => {
-  return (
-    <div> {Data.map((items, index) => { return (<div>{items}</div>) })}  </div>
-  )
-}
 
-export default NewAccordion
+  const [selectedId, setSelectedId] = useState([])
+  const [SingleSelection, setSingleSelection] = useState(true)
+
+  function SwitchButton() {
+    setSingleSelection(!SingleSelection)
+  }
+
+
+  function HandleSingleSelection(currentId) {
+    setSelectedId(selectedId.includes(currentId) ? [] : [currentId])
+    console.log(currentId)
+  }
+
+
+  function HandleMultiSelection(currentId) {
+
+    if (selectedId.includes(currentId)) {
+      setSelectedId(selectedId.filter((id) => id !== currentId))
+    }
+    else {
+      setSelectedId([...selectedId, currentId])
+    }
+  }
+  return (
+    <div>
+      <button onClick={SwitchButton}>{SingleSelection ? 'MultiSelection': 'SingleSelection'  } </button>
+
+      {Data.map((items, index) => {
+        return (
+          <div key={index}>
+            <div>{items.id}</div>
+            <div>{items.question}</div>
+            <div onClick={SingleSelection?() => HandleSingleSelection(items.id) : ()=> HandleMultiSelection(items.id) } >+</div>
+            <div> {selectedId.includes(items.id) ? items.answer : []}</div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default NewAccordion;
