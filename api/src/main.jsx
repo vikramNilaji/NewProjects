28a11560b78229e2a9ad42064c2fefdf5347937a
Vikram Nilaji <97115025+vikramNilaji@users.noreply.github.com>
2025-08-14 06:35:50 +0530

    add

--- api/src/main.jsx
+++ api/src/main.jsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import App2 from './App2.jsx'
+import App3 from './App3.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-    <App2/>
+    {/* <App /> */}
+    {/* <App2/> */}
+    <App3/>
   </StrictMode>,
 )
